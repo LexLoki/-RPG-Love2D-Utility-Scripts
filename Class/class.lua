1fdd78085ac405e879c93cc010d1675cdce11401
LexLoki <pietroribeiropepe@hotmail.com>
2016-02-11 02:32:35 -0200

    class 2.0

--- Class/class.lua
+++ Class/class.lua
@@ -1,51 +1,27 @@
---[[
-Script to simulate class and inheritance in Lua
-How to use:
-
-*Initialize a base class using:
-  myClass = class_new("myClassName")
-When creating a new instance, you need to first initialize it with:
-  local self = myClass.newObject()
-If you pretend to subClass the class, implement the instance creation method as .new
-Then you can set your properties
-
-*Initialize a subclass (of myClass):
-  subClass = class_extends(myClass,"subClassName")
-When creating a new instance, you need to first initialize it:
-  local self = subClass.newObject(PASS HERE THE PARAMETERS OF THE SUPERCLASS INIT)
-
-*Availables methods for an instance of a class created with a script
-  :class() - Returns the class of the instance
-  :name() - Returns the name of the instance's class, or "unknown" if no name was given
-  :superClass() - Returns the superClass of the instance's class, or NIL if it doesn't have one
-  :superClasses() - Returns an array with all the superClassses
-  :is_a(class) - Checks if the instance is of the given class, or a subclass of it
-  
-*Example
-
-myClass = class_new("myClassName")
-function myClass.new(a,b,c)
-  local self = myClass.newObject()
-  self.a = a
-  self.b = b
-  self.c = c
-end
-
-subClass = class_extends(myClass,"myClassName")
-function subClass.new(a)
-  local self = subClass.new(a,a*2,a*3)
-  return self
-end
-]]
-
 function class_new(name)
   local new_class = {}
-  new_class.__index = new_class
   function new_class.newObject(...)
     local sup = new_class:superClass()
-    local newinst = sup == nil and {} or sup.new(...)
-    --local newinst = {}
-    setmetatable(newinst, new_class)
+    local newinst = {}
+    local meta = {}
+    if sup == nil then
+      meta.__index = new_class
+    else
+      newinst.super = sup.new(...)
+      function meta.__index(table,key)
+        local classGet = rawget(new_class,key)
+        local superGet = rawget(newinst.super,key)
+        return (superGet or classGet or newinst.super[key])
+      end
+      function meta.__newindex(table, key, value)
+        if newinst.super[key] == nil then
+          rawset(table,key,value)
+        else
+          newinst.super[key] = value
+        end
+      end
+    end
+    setmetatable(newinst, meta)
     return newinst
   end
   new_class.new = new_class.newObject
@@ -84,7 +60,7 @@ end
 function class_extends(baseClass,name)
   local new_class = class_new(name)
   if baseClass~=nil then 
-    setmetatable(new_class,baseClass)
+    setmetatable(new_class,{__index=baseClass})
     function new_class:superClass()
       return baseClass
     end
