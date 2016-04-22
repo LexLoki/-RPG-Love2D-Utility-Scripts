3d478870e85cb72838e1d46c03bc017a9d4dafa6
LexLoki <pietroribeiropepe@hotmail.com>
2016-04-22 16:17:17 -0300

    Parallax

--- Parallax/parallax.lua
+++ Parallax/parallax.lua
@@ -1,64 +1,30 @@
+--  The MIT License (MIT)
+--  Copyright © 2016 Pietro Ribeiro Pepe.
 
+--  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+--  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
---[[ 
+-- Script to help implementing Parallax background
+-- Version 2.0 - 2016/04/22
 
-Requires vector of maps V with each map having:
-  - image: The image of the plane
-  - speed: Speed of the moving map (tiles per second)
-  - width: Width desirable for the map, in points
-  - height: Width desirable for the map, in points
+Parallax = {}
 
-The order is important, the deepest maps first
-  
-  example code:
-  
-  (GLOBAL)
-  local parallaxBackground
-  
-  (LOAD)
-  local screenWidth = love.graphics.getWidth()
-  local screenHeight = love.graphics.getHeight()
-  local planoDeep = {
-    image = love.graphics.newImage("planoDeep.png"),
-    speed = screenWidth/5,
-    width = screenWidth,
-    height = screenHeight
-  }
-  local planoBackground = {
-    image = love.graphics.newImage("planoBackground.png"),
-    speed = screenWidth/2,
-    width = screenWidth,
-    height = screenHeight
-  }
-  local mapsVector = {planoDeep, planoBackground}
-  parallaxBackground = parallax_new(mapsVector)
-  
-  (UPDATE)
-  -- IF YOU WANT TO MOVE WITH THE DEFAULT SPEED, DOESN NOT NEED THE PARAMETER mov
-  parallax_update(dt, parallaxBackground)
-  -- MOV is to change somehow the SPEED, for example, make the parallax move 50% slower -> mov = 0.5
-  parallax_update(dt, p, 0.5)
-  
-  (DRAW)
-  parallax_draw(parallaxBackground)
-]]
-
-local updateWithVel
-
-function parallax_new(listOfMaps)
+function Parallax.new(listOfMaps)
+  local self = {}
+  setmetatable(self,{__index=Parallax})
   for i,v in ipairs(listOfMaps) do
-    v.position = 0
-    v.scale = {
-      x = v.width / v.image:getWidth(),
-      y = v.height / v.image:getHeight()
-    }
+    local t = {position=0,scale={x = v.width / v.image:getWidth(),
+      y = v.height / v.image:getHeight()}}
+    for j,w in pairs(v) do t[j] = w end
+    table.insert(self,t)
   end
-  return {list = listOfMaps}
+  return self
 end
 
-function parallax_update(dt, p, mov)
+function Parallax:update(dt, mov)
   if mov == nil then mov = 1 end
-  for i,v in ipairs(p.list) do
+  for i,v in ipairs(self) do
     v.position = v.position + v.speed*mov*dt
     if v.position > v.width then
       v.position = v.position%v.width
@@ -66,8 +32,8 @@ function parallax_update(dt, p, mov)
   end
 end
 
-function parallax_draw(p)
-  for i,v in ipairs(p.list) do
+function Parallax:draw()
+  for i,v in ipairs(self) do
     local pos = v.position
     love.graphics.draw(v.image, -pos, 0, 0, v.scale.x, v.scale.y)
     love.graphics.draw(v.image, v.width-pos, 0, 0, v.scale.x, v.scale.y)
